b9c94b513e1c42e3381aca20b91fb7e9554cff39
ben214 <80927447+ben214@users.noreply.github.com>
2021-04-12 23:39:30 +0300

    new styling and payment page

--- payment.js
+++ payment.js
@@ -1,3 +1,4 @@
-function paypop() {
-  $("#paypop").css("visibility", "visible");
+function paypop(){
+   $("#paypop").css({opacity: 0.0, display: "content"}).animate({opacity: 1}, 1800);
+   $("iframe").animate({height: "1300px", width: "400px"}).css("margin", "0 auto");
 }
