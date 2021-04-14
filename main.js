42b2e27a20fef76b669493ea4576b9cbc3274556
ben214 <80927447+ben214@users.noreply.github.com>
2021-04-14 23:06:57 +0300

    more stuff

---
+++ main.js
@@ -0,0 +1,9 @@
+function baropen(){
+  $("#rightarr").css("opacity", "0");
+  $(".rightnav").css("right", 0)
+}
+
+function barclose(){
+  $("#rightarr").css("opacity", "1");
+  $(".rightnav").css("right", "-14em");
+}
