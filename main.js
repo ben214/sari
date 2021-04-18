2daa352adfbc9163db47370cfcea94087e10497c
ben214 <80927447+ben214@users.noreply.github.com>
2021-04-18 21:16:11 +0300

    new styling, mobile version, backgrounds

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-function baropen(){
-  $("#rightarr").css("opacity", "0");
-  $(".rightnav").css("right", 0)
-}
 
-function barclose(){
-  $("#rightarr").css("opacity", "1");
-  $(".rightnav").css("right", "-11em");
-}
