33b3c5acf3665dd76f71f4c8ae82edbe100017ff
ben214 <80927447+ben214@users.noreply.github.com>
2021-04-22 23:45:49 +0300

    responsiveness

--- payment.js
+++ payment.js
@@ -1,4 +1,4 @@
 function paypop(){
-   $("#paypop").css({opacity: 0.0, display: "block"}).animate({opacity: 1}, 1800);
-   $("iframe").animate({height: "1300px", width: "400px"}).css("margin", "0 auto");
+   $("#paypop").css({opacity: 0.0, display: "flex"}).animate({opacity: 1}, 1800);
+
 }
