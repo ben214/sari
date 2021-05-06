d78226b99ebeac9f4ea788f87a8e63725aff1f05
ben214 <80927447+ben214@users.noreply.github.com>
2021-05-07 00:34:07 +0300

    almost ready for firs stage

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
 
+
+var i = 0;
+setInterval(moveIt, 3500);
+  var moveIt = function slideUs(){
+
+ var active = document.getElementsByClassName('slide');
+ var max = active.length;
+ var prev = active[i];
+ var playing = active[i +1];
+   console.log("before "+i);
+
+   if (i == max-1) {
+      i=0;
+      playing = active[max-1];
+      prev = active[i];
+
+      playing.classList.remove("active");
+      
+      console.log("restart!!" + active[max-1].style.display);
+      playing = active[0];
+
+   }else {
+     i++;
+     prev.classList.remove("active");
+   }
+
+   playing.classList.add("active");
+   console.log("after "+ i);
+}
+
+
+// if (i === max-1) {
+//   i=0;
+//   playing = active[i];
+//   console.log( "STARTINg AGAIN!!!");
+// }else{
+//
+//   i++;
+//   console.log(active[max-1]);
+// }
