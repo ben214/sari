462fa3147210256678756309861d7a35d5cbc180
ben214 <80927447+ben214@users.noreply.github.com>
2021-06-21 20:25:13 +0300

    complete for now with new mamar page, functions to alter breadcrumbs and readhead as mamarim

--- main.js
+++ main.js
@@ -44,7 +44,6 @@ setInterval(moveIt, 3500);
 
   function openthis(){
      closeAll()
-
       $(this).find('.toggling').show(100),
       $(this).children(".read").children('i').toggleClass("rotate", 1000)
       console.log('block!!');
@@ -52,41 +51,61 @@ setInterval(moveIt, 3500);
         $(this).on('click', closeAll)
 }
 
+
+// adding new nav buttons to sidebar---------------------------------------------------
+
+
+  var toggling = $('.toggling')
+  var esektoggle = toggling[2]
+  var paturtoggle = $(esektoggle).children('a')
+  var paturA = paturtoggle[1]
+    $(paturA).before("<a href='hevdelPaturMurshe.html' class='maanakAsk read-inner1'>ההבדלים בין עוסק פטור לעוסק מורשה<i class='fa fa-chevron-circle-left'></i></a>")
+  var mursheA = paturtoggle[1]
+  $(mursheA).after("<a href='hazHon.html' class='maanakAsk read-inner1'>הצהרת הון<i class='fa fa-chevron-circle-left'></i></a>")
+
+var readingTop = $('#reading')
+var thisPage = $('h1').html()
+$(readingTop).prepend('<p  id="Bcrumbs"><a href="index.html"><i class="fa fa-home"></i>בית</a> / <a href="mamarim.html">מאמרים</a> / <a class="thispage"></a></p>')
+$('a.thispage').text(thisPage);
+console.log($('h1').html());
+var readhead = $('#readingBar>.readHead')
+
+$(readhead).text("מאמרים:");
+$(readhead).append("<a style='height: 100%; width: 100%; cursor: pointer; position: absolute' href='mamarim.html'></a>")
+console.log($(readhead).text());
+  //end of adding new nav buttons to sidebar---------------------------------------------------
+
 $(document).ready(function(){
-
-  console.log($('.read-inner1').css('fontFamily'));
-
-$('.bottomarr').click(clsallansw)
-$('.quest').click(openanswer)
-function openanswer() {
+   $('.bottomarr').click(clsallansw)
+  $('.quest').click(openanswer)
+ function openanswer() {
   var answer = $(this).parents('.questlist').children('.answ')
   clsallansw()
   $(this).children('.arrowbox').addClass('arrowboxopen'),
-  answer.slideDown(300),
+  answer.show(300),
   $(this).attr('id', 'activequest')
   $(this).unbind('click', openanswer)
-    // $('html, body').animate({
-    //    scrollTop: $('#activequest').offset().top
-    //  }, 300)
-    window.scrollTo(0, 180)
+    $('html, body').animate({
+       scrollTop: $('#activequest').offset().top
+     }, 300)
+     var mq = window.matchMedia( "(max-width: 600px)" );
+  if (mq.matches){
+      $('.head-title2').css('display', 'none')
+      }
   $(this).click(clsallansw)
   console.log('openanswer')
-  console.log($('#activequest').length);
+  console.log($('#activequest').css('background'));
 }
 function clsallansw(){
    $('.answ').css('display', 'none'),
    $('.quest').unbind('click', clsallansw),
    $('.quest').click(openanswer)
    $('.arrowbox').removeClass('arrowboxopen')
+   var mq = window.matchMedia( "(max-width: 600px)" );
+if (mq.matches){
+    $('.head-title2').css('display', 'block')
+    }
 
 }
-// function closeanswer() {
-//   $('.quest').click(openanswer)
-//   $('.answ').hide(300),
-//   $('.arrowbox').removeClass('arrowboxopen')
-//   $('.quest').unbind('click', closeanswer)
-//   console.log('closeanswer')
-// }
-
 
 })
--- payment.js
+++ payment.js
@@ -1,4 +1,6 @@
 function paypop(){
-   $("#paypop").css({opacity: 0.0, display: "flex"}).animate({opacity: 1}, 1800);
-
-}
+   $("#paypop").css({opacity: 0.0, display: "flex"}).animate({opacity: 1}, 400),
+   $('html, body').animate({
+      scrollTop: $("#paypop").offset().top
+    }, 400)
+    }
